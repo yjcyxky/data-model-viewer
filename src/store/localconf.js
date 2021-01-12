bcab436612264b01d4ae8dee63cedc79b3b088fc
YJC <yjcyxky@163.com>
2021-01-12 13:29:14 +0800

    Reset base url.

--- src/store/localconf.js
+++ src/store/localconf.js
@@ -1,5 +1,10 @@
-const dataDictionaryTemplatePath = "data/";
-const dictionaryApiPath = "data/";
+// Development
+// const dataDictionaryTemplatePath = "data/";
+// const dictionaryApiPath = "data/";
+
+// Production
+const dataDictionaryTemplatePath = "http://service.3steps.cn/attachments/data-model/";
+const dictionaryApiPath = "http://service.3steps.cn/attachments/data-model/";
 
 // Commonjs style export, so can load from nodejs into data/gqlSetup
 module.exports = {
